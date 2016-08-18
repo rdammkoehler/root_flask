9cd9b75ba94cb4b9986f2b90e954c08b464caba9
Rich Dammkoehler <rpd@noradltd.com>
2016-08-18 12:26:22 -0500

    correction

--- examples/fortune/app.py
+++ examples/fortune/app.py
@@ -1,6 +1,6 @@
 from injector import Injector
 
-from n.s.f.app_module import APP
-from .fortune import FortuneModule
+from n.s.f.app_module import AppModule
+from .fortune import FortuneModule, FORTUNE
 
-app = Injector(FortuneModule).get(APP)
+app = Injector([AppModule, FortuneModule]).get(FORTUNE)
